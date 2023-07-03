522d01032d8cd6cbbbfccb8b9b8db4faaee8a634
James Ma <jamesma100@gmail.com>
2023-07-03 15:06:58 -0700

    probuf + master rpc setup

--- src/bin/master.rs
+++ src/bin/master.rs
@@ -1,6 +1,39 @@
 use std::{thread, time};
 use colored::Colorize;
-pub mod debug;
+use tonic::{transport::Server, Request, Response, Status};
+use tasks::task_server::{Task, TaskServer};
+use tasks::{TaskRequest, TaskResponse};
+
+// types tonic generated based on proto/tasks.proto
+// so the `use` statements can reference them
+pub mod tasks {
+	tonic::include_proto!("tasks");
+}
+
+#[derive(Debug, Default)]
+pub struct TaskService {}
+
+#[tonic::async_trait]
+impl Task for TaskService {
+	async fn send_task(
+		&self,
+		request: Request<TaskRequest>,
+	) -> Result<Response<TaskResponse>, Status> {
+		println!("master got a request: {:?}", request);
+
+		let req = request.into_inner();
+
+		let reply = TaskResponse {
+			file_name: "sample_file".to_string(),
+			task_num: 1,
+			n_reduce: 3,
+			n_map: 4,
+			is_map: true,
+		};
+
+		Ok(Response::new(reply))
+	}
+}
 
 
 #[derive(Debug)]
@@ -18,6 +51,17 @@ impl Master {
 	pub fn done(&self) -> bool {
 		self.done
 	}
+
+	pub async fn boot(&self) -> Result<(), Box<dyn std::error::Error>> {
+		let addr = "[::1]:50051".parse()?;
+		let task_service = TaskService::default();
+
+		Server::builder()
+			.add_service(TaskServer::new(task_service))
+			.serve(addr)
+			.await?;
+		Ok(())
+	}
 } 
 
 fn main() {
@@ -27,6 +71,7 @@ fn main() {
 		"mymaster".to_string(),
 		false,
 	);
+	master.boot();
 	println!("{}: {}", "master created".bright_red(), master.name.bright_red());
 	
 	let five_seconds = time::Duration::from_millis(5000);
