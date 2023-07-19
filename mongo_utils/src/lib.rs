886ca11b7653f0d8e5d5e6bf18f289ec6802cb3e
James Ma <jamesma100@gmail.com>
2023-07-18 22:06:51 -0700

    add mongo tests

--- mongo_utils/src/lib.rs
+++ mongo_utils/src/lib.rs
@@ -15,17 +15,30 @@ struct MasterState {
 // creates a mongodb collection
 async fn create_collection(client: &Client, db_name: &str, coll_name: &str) {
     let db = client.database(db_name);
+    for collection_name in db.list_collection_names(None).await.unwrap() {
+        if collection_name == coll_name {
+            eprintln!("Collection {} already exists. Exiting.", collection_name);
+            return
+        };
+    }
     db.create_collection(coll_name, None).await.unwrap();
 }
 
+// drops a mongodb collection
+async fn drop_collection(client: &Client, db_name: &str, coll_name: &str) {
+    let db = client.database(db_name);
+    let coll = db.collection::<mongodb::bson::Document>(coll_name);
+    coll.drop(None);
+}
+
 // initializes master state
-async fn init_state(client: &Client, db_name: &str, coll_name: &str, name: &str, n_map: i64, n_reduce: i64) {
+async fn init_state(client: &Client, db_name: &str, coll_name: &str, record_name: &str, n_map: i64, n_reduce: i64) {
     let db = client.database(db_name);
     let coll = db.collection(coll_name);
 
     coll.insert_one(
         MasterState { 
-            name: name.to_string(),
+            name: record_name.to_string(),
             n_map: n_map,
             n_reduce: n_reduce,
             map_tasks_left: n_map
@@ -35,11 +48,11 @@ async fn init_state(client: &Client, db_name: &str, coll_name: &str, name: &str,
 }
 
 // gets value of some field of the current state
-async fn get_state(client: &Client, db_name: &str, coll_name: &str, name: &str, field: &str) -> Option<i64> {
+async fn get_val(client: &Client, db_name: &str, coll_name: &str, record_name: &str, field: &str) -> Option<i64> {
     let db = client.database(db_name);
     let coll = db.collection::<mongodb::bson::Document>(coll_name);
 
-    let filter = doc! {"name": name.to_string()};
+    let filter = doc! {"name": record_name.to_string()};
     let res = coll.find_one(Some(filter), None).await.unwrap();
 
     match res {
@@ -51,11 +64,48 @@ async fn get_state(client: &Client, db_name: &str, coll_name: &str, name: &str, 
 }
 
 // updates some integer count in the current state
-async fn update_count(client: &Client, db_name: &str, coll_name: &str, name: &str, field: &str, new_val: i64) {
+async fn update_count(client: &Client, db_name: &str, coll_name: &str, record_name: &str, field: &str, new_val: i64) {
     let db = client.database(db_name);
     let coll = db.collection::<mongodb::bson::Document>(coll_name);
 
-    let filter = doc! {"name": name.to_string()};
+    let filter = doc! {"name": record_name.to_string()};
     let update = doc! {"$set": {field.to_string(): new_val}};
     coll.update_one(filter, update, None).await.unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_initialization() {
+        let addr = "[::1]:50051";
+        let client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
+        let client = Client::with_options(client_options).unwrap();
+        let db_name = "test_db";
+        let coll_name = "test_coll";
+
+        create_collection(&client, db_name, coll_name).await;
+        init_state(&client, db_name, coll_name, "test_state", 10, 11).await;
+        assert_eq!(get_val(&client, db_name, coll_name, "test_state", "n_map").await.unwrap(), 10);
+        assert_eq!(get_val(&client, db_name, coll_name, "test_state", "n_reduce").await.unwrap(), 11);
+
+        drop_collection(&client, db_name, coll_name).await;
+    }
+
+    #[tokio::test]
+    async fn test_update_count() {
+        let addr = "[::1]:50051";
+        let client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
+        let client = Client::with_options(client_options).unwrap();
+        let db_name = "test_db";
+        let coll_name = "test_coll";
+
+        create_collection(&client, db_name, coll_name).await;
+        init_state(&client, db_name, coll_name, "test_update", 15, 1).await;
+        update_count(&client, db_name, coll_name, "test_update", "n_map", 34).await;
+        assert_eq!(get_val(&client, db_name, coll_name, "test_update", "n_map").await.unwrap(), 34);
+
+        drop_collection(&client, db_name, coll_name).await;
+    }
 }
